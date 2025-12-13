01e6682f7a76b2d2187fa5d880272a62083382e3
mohitpatel-18 <152364977+mohitpatel-18@users.noreply.github.com>
2025-12-13 10:12:49 +0530

    fix: recreate Navbar with correct casing for render

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbarbar";
 import Hero from "./components/Hero";
 import Tools from "./pages/Tools";
 import Contact from "./pages/Contact";
 import Profile from "./pages/Profile";
-import Reviews from "./components/Reviews"; // <- new
+import Reviews from "./components/Reviews";
 
 import MyBookings from "./pages/MyBookings";
 import RentRequests from "./pages/RentRequests";
